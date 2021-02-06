189b3c1516cf08bbaeee9130068eb52b83c77823
Tristan Hörler <tristan.horler@gmail.com>
2021-02-06 15:03:32 +0100

    add seed to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+10.times do
+  article = Article.new()
+  article.title = Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 4)
+  article.content = Faker::Lorem.paragraphs(number: 1)
+  article.save
+end
+
